e77813e1ea741aa69ddb83d2e2709a4855c4faf0
Kun Hwi Ko <khk305@stern.nyu.edu>
2020-06-03 23:22:41 -0400

    fix web parse

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from py_folders.websearch import WebSearch
 
 def main():
     ws = WebSearch("https://finance.yahoo.com/gainers",5)
+    ws.web_parse()
 
 if __name__ == "__main__":
     main()
--- py_folders/websearch.py
+++ py_folders/websearch.py
@@ -1,12 +1,19 @@
 import requests
+import urllib.request, urllib.parse, urllib.error
+from bs4 import BeautifulSoup
 
 class WebSearch():
 
     def __init__(self, website, count):
+        # website represents the site to scrap
+        # count represents the number of companies to retrieve 
         self.website = website 
         self.count = count 
 
     def web_parse(self):
-        web_request = requests.get(self.website)
-        html = web_request.text
-        print(html) 
+        # retrieve website to parses 
+        url = requests.get(self.website)
+        html = urllib.request.urlopen(url).read()
+        soup = BeautifulSoup(html,"html.parser")
+
+        

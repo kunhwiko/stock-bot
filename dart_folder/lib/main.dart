aae700ed456d8b0bcb468f9ad366d295fa831d18
Kun Hwi Ko <khk305@stern.nyu.edu>
2020-06-09 19:04:08 -0400

    create web app interface

--- dart_folder/lib/main.dart
+++ dart_folder/lib/main.dart
@@ -1,6 +1,40 @@
 import 'package:flutter/material.dart';
 
 void main() => runApp(MaterialApp(
-  home: Text('Stock Bot'),
+  home : Home(),
 ));
+
+class Home extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+
+      appBar: AppBar(
+        title : Text(
+            'Stock Bot',
+            style : TextStyle(
+              fontSize : 20.0,
+              fontWeight : FontWeight.bold,
+              letterSpacing : 2.0,
+              color : Colors.grey[300],
+            )
+        ),
+        centerTitle: true,
+        backgroundColor: Colors.red[300],
+      ),
+
+      body : Center(
+        child : Text("This is stock bot!"),
+      ),
+
+      floatingActionButton: FloatingActionButton(
+        onPressed : () {},
+        child : Text("click"),
+        backgroundColor : Colors.red[300],
+      ),
+
+    );
+  }
+}
+
 

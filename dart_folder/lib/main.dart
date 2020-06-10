3d22f2c6e6b1288ce2e04435d72d0232e9e859e1
Kun Hwi Ko <khk305@stern.nyu.edu>
2020-06-09 22:25:14 -0400

    Practice with Flutter

--- dart_folder/lib/main.dart
+++ dart_folder/lib/main.dart
@@ -8,33 +8,76 @@ class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor : Colors.grey[900],
+      appBar : AppBar(
+        title : Text('Stock Bot'),
+        centerTitle : true,
+        backgroundColor : Colors.grey[850],
+        elevation : 0.0,
+      ),
 
-      appBar: AppBar(
-        title : Text(
-            'Stock Bot',
-            style : TextStyle(
-              fontSize : 20.0,
-              fontWeight : FontWeight.bold,
-              letterSpacing : 2.0,
-              color : Colors.grey[300],
-            )
+      body : Padding(
+        padding : EdgeInsets.fromLTRB(30.0, 40.0, 30.0, 0.0),
+        child : Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children : <Widget>[
+            Text(
+              'NAME',
+              style : TextStyle(
+                color : Colors.grey,
+                letterSpacing: 2.0,
+              ),
+            ),
+            SizedBox(height : 10.0),
+            Text(
+              'Kun Hwi Ko',
+              style : TextStyle(
+                color : Colors.amberAccent[200],
+                letterSpacing : 2.0,
+                fontSize : 20.0,
+                fontWeight : FontWeight.bold,
+              ),
+            ),
+            SizedBox(height : 30.0),
+            Text(
+              'School',
+              style : TextStyle(
+                color : Colors.grey,
+                letterSpacing: 2.0,
+              ),
+            ),
+            SizedBox(height : 10.0),
+            Text(
+              'University of Pennsylvania',
+              style : TextStyle(
+                color : Colors.amberAccent[200],
+                letterSpacing : 2.0,
+                fontSize : 20.0,
+                fontWeight : FontWeight.bold,
+              ),
+            ),
+            SizedBox(height : 30.0),
+            Row(
+              children : <Widget>[
+                Icon(
+                  Icons.email,
+                  color : Colors.grey[400],
+                ),
+                SizedBox(width:10.0),
+                Text(
+                  'kunko@seas.upenn.edu',
+                  style : TextStyle(
+                    color : Colors.grey[400],
+                    fontSize : 10.0,
+                    letterSpacing : 1.0,
+                  ),
+                ),
+              ],
+            ),
+          ],
         ),
-        centerTitle: true,
-        backgroundColor: Colors.red[300],
-      ),
-
-      body : Center(
-        child : Text("This is stock bot!"),
-      ),
-
-      floatingActionButton: FloatingActionButton(
-        onPressed : () {},
-        child : Text("click"),
-        backgroundColor : Colors.red[300],
       ),
 
     );
   }
 }
-
-

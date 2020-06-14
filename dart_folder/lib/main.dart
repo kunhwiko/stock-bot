2dd71ca2516f18bd52e612e9b9bdf67f6776ea52
Kun Hwi Ko <khk305@stern.nyu.edu>
2020-06-14 17:10:39 -0400

    implement futurebuilder

--- dart_folder/lib/main.dart
+++ dart_folder/lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'parseJson.dart';
 import 'dart:convert';
 import 'dart:async' show Future;
+import 'dart:io';
 import 'package:flutter/services.dart' show rootBundle;
 
 void main(){
@@ -14,181 +15,183 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+
+  Future<List<Stock>> parseJson() async{
+    String text = await rootBundle.loadString('assets/data.json');
+    List<dynamic> decodedList = json.decode(text);
+    List<Stock> data = [null,null,null,null,null];
+    for(int i = 0; i < 5; i++){
+      data[i] = Stock.fromJson(decodedList[i]);
+    }
+    return data;
+  }
+
   @override
   Widget build(BuildContext context) {
-
-    List<String> symbols = [null, null, null, null, null];
-    List<String> open = [null, null, null, null, null];
-    List<String> close = [null, null, null, null, null];
-
-    parseJson() async{
-      String text = await rootBundle.loadString('assets/data.json');
-      List<dynamic> decodedList = json.decode(text);
-      List<Stock> stockData = [null, null, null, null, null];
-
-      for (int i = 0; i < decodedList.length; i++) {
-        Stock stock = new Stock.fromJson(decodedList[i]);
-        stockData[i] = stock;
-      }
-    }
-
-    parseJson();
-
     return MaterialApp(
       home: DefaultTabController(
         length: 5,
-        child: new Scaffold(
-          body: TabBarView(
-            children: [
-              new Column(
-                children: [
-                  Row(
-                    mainAxisAlignment : MainAxisAlignment.center,
-                    children : <Widget>[
-                      Expanded(
-                        child : Container(
-                          padding : EdgeInsets.fromLTRB(20.0, 40.0, 20.0, 25.0),
-                          child: Text('Stock'),
-                          color : Colors.red[200],
+        child: FutureBuilder<List<Stock>>(
+          future : parseJson(),
+          builder : (context,snapshot) {
+            if (snapshot.hasData && snapshot.data != null){
+              return new Scaffold(
+                body: TabBarView(
+                  children: [
+                    new Column(
+                      children: [
+                        Row(
+                          mainAxisAlignment : MainAxisAlignment.center,
+                          children : <Widget>[
+                            Expanded(
+                              child : Container(
+                                padding : EdgeInsets.fromLTRB(20.0, 40.0, 20.0, 25.0),
+                                child: Text('Stocks'),
+                                color : Colors.red[200],
+                              ),
+                            ),
+                          ],
                         ),
-                      ),
-                    ],
-                  ),
-                  Row(
-                    children : <Widget>[
-                      Expanded(
-                        child : Container(
-                          padding : EdgeInsets.fromLTRB(20.0, 15.0, 20.0, 10.0),
-                          color : Colors.white,
+                        Row(
+                          children : <Widget>[
+                            Expanded(
+                              child : Container(
+                                padding : EdgeInsets.fromLTRB(20.0, 15.0, 20.0, 10.0),
+                                color : Colors.white,
+                              ),
+                            ),
+                          ],
                         ),
-                      ),
-                    ],
-                  ),
-                  Container(
-                    child : Image(
-                      image : AssetImage('assets/image1.png'),
-                    ),
-                    color : Colors.white,
-                  ),
-                  Row(
-                    children : <Widget>[
-                      Expanded(
-                        child : Container(
-                          padding : EdgeInsets.fromLTRB(20.0, 0.0, 20.0, 25.0),
-                          color : Colors.white,
-                        ),
-                      ),
-                    ],
-                  ),
-                  Row(
-                    children : <Widget>[
-                      Expanded(
-                        child : Container(
-                          padding : EdgeInsets.fromLTRB(20.0, 15.0, 20.0, 15.0),
-                          child: Text('    Profit  :  '),
-                          color : Colors.white,
-                        ),
-                      ),
-                      Expanded(
-                        child : Container(
-                          color : Colors.white,
-                          child: IconButton(
-                            onPressed: (){},
-                            icon : Icon(Icons.assessment),
-                            color : Colors.red[200],
+                        Container(
+                          child : Image(
+                            image : AssetImage('assets/image1.png'),
                           ),
-                        ),
-                      ),
-                    ],
-                  ),
-                  Row(
-                    children : <Widget>[
-                      Expanded(
-                        child : Container(
-                          padding : EdgeInsets.fromLTRB(20.0, 30.0, 20.0, 30.0),
                           color : Colors.white,
                         ),
-                      ),
-                    ],
-                  ),
-                ],
-              ),
-              new Column(
-                children: [
-                  Row(
-                    mainAxisAlignment : MainAxisAlignment.center,
-                    children : <Widget>[
-                      Expanded(
-                        child : Container(
-                          padding : EdgeInsets.fromLTRB(20.0, 40.0, 20.0, 25.0),
-                          child: Text('Stock 2'),
-                          color : Colors.orange[200],
+                        Row(
+                          children : <Widget>[
+                            Expanded(
+                              child : Container(
+                                padding : EdgeInsets.fromLTRB(20.0, 0.0, 20.0, 25.0),
+                                color : Colors.white,
+                              ),
+                            ),
+                          ],
                         ),
-                      ),
-                    ],
-                  ),
-                  Row(
-                    children : <Widget>[
-                      Expanded(
-                        child : Container(
-                          padding : EdgeInsets.fromLTRB(20.0, 15.0, 20.0, 10.0),
-                          color : Colors.white,
+                        Row(
+                          children : <Widget>[
+                            Expanded(
+                              child : Container(
+                                padding : EdgeInsets.fromLTRB(20.0, 15.0, 20.0, 15.0),
+                                child: Text('    Profit  :  '),
+                                color : Colors.white,
+                              ),
+                            ),
+                            Expanded(
+                              child : Container(
+                                color : Colors.white,
+                                child: IconButton(
+                                  onPressed: (){},
+                                  icon : Icon(Icons.assessment),
+                                  color : Colors.red[200],
+                                ),
+                              ),
+                            ),
+                          ],
                         ),
-                      ),
-                    ],
-                  ),
-                  Container(
-                    child : Image(
-                      image : AssetImage('assets/image2.png'),
+                        Row(
+                          children : <Widget>[
+                            Expanded(
+                              child : Container(
+                                padding : EdgeInsets.fromLTRB(20.0, 30.0, 20.0, 30.0),
+                                color : Colors.white,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ],
                     ),
-                    color : Colors.white,
-                  ),
-                  Row(
-                    children : <Widget>[
-                      Expanded(
-                        child : Container(
-                          padding : EdgeInsets.fromLTRB(20.0, 35.0, 20.0, 100.0),
+                    new Column(
+                      children: [
+                        Row(
+                          mainAxisAlignment : MainAxisAlignment.center,
+                          children : <Widget>[
+                            Expanded(
+                              child : Container(
+                                padding : EdgeInsets.fromLTRB(20.0, 40.0, 20.0, 25.0),
+                                child: Text('Stock 2'),
+                                color : Colors.orange[200],
+                              ),
+                            ),
+                          ],
+                        ),
+                        Row(
+                          children : <Widget>[
+                            Expanded(
+                              child : Container(
+                                padding : EdgeInsets.fromLTRB(20.0, 15.0, 20.0, 10.0),
+                                color : Colors.white,
+                              ),
+                            ),
+                          ],
+                        ),
+                        Container(
+                          child : Image(
+                            image : AssetImage('assets/image2.png'),
+                          ),
                           color : Colors.white,
                         ),
-                      ),
-                    ],
-                  ),
-                ],
-              ),
-              new Container(
-                color: Colors.lightGreen,
-              ),
-              new Container(
-                color: Colors.lightBlue[300],
-              ),
-              new Container(
-                color: Colors.deepPurple[100],
-              ),
-            ],
-          ),
-          bottomNavigationBar: new TabBar(
-            tabs: [
-              Tab(
-                icon: new Icon(Icons.filter_1),
-              ),
-              Tab(
-                icon: new Icon(Icons.filter_2),
-              ),
-              Tab(
-                icon: new Icon(Icons.filter_3),
-              ),
-              Tab(
-                icon: new Icon(Icons.filter_4),
-              ),
-              Tab(icon: new Icon(Icons.filter_5),)
-            ],
-            labelColor: Colors.black,
-            unselectedLabelColor: Colors.grey[500],
-            indicatorSize: TabBarIndicatorSize.label,
-            indicatorPadding: EdgeInsets.all(5.0),
-            indicatorColor: Colors.white,
-          ),
-          backgroundColor: Colors.grey[100],
+                        Row(
+                          children : <Widget>[
+                            Expanded(
+                              child : Container(
+                                padding : EdgeInsets.fromLTRB(20.0, 35.0, 20.0, 100.0),
+                                color : Colors.white,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
+                    new Container(
+                      color: Colors.lightGreen,
+                    ),
+                    new Container(
+                      color: Colors.lightBlue[300],
+                    ),
+                    new Container(
+                      color: Colors.deepPurple[100],
+                    ),
+                  ],
+                ),
+                bottomNavigationBar: new TabBar(
+                  tabs: [
+                    Tab(
+                      icon: new Icon(Icons.filter_1),
+                    ),
+                    Tab(
+                      icon: new Icon(Icons.filter_2),
+                    ),
+                    Tab(
+                      icon: new Icon(Icons.filter_3),
+                    ),
+                    Tab(
+                      icon: new Icon(Icons.filter_4),
+                    ),
+                    Tab(icon: new Icon(Icons.filter_5),)
+                  ],
+                  labelColor: Colors.black,
+                  unselectedLabelColor: Colors.grey[500],
+                  indicatorSize: TabBarIndicatorSize.label,
+                  indicatorPadding: EdgeInsets.all(5.0),
+                  indicatorColor: Colors.white,
+                ),
+                backgroundColor: Colors.grey[100],
+              );
+            }else{
+              return Center(child:CircularProgressIndicator());
+            }
+          }
         ),
       ),
     );

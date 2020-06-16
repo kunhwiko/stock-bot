80ea3a139733c78f6ea65d09818bf290f6ad1067
Kun Hwi Ko <khk305@stern.nyu.edu>
2020-06-16 04:32:41 -0400

    fixes to main.py

--- py_folder/main.py
+++ py_folder/main.py
@@ -3,6 +3,8 @@ import time
 import json
 
 def main():
+    # 'finance.yahoo.com/gainers' for most gaining stocks
+    # 'finance.yahoo.com/most-active' for most active stocks
     ws = WebSearch("https://finance.yahoo.com/gainers?count=100")
     symbols, changes = ws.web_parse()
     sorted_symbols = ws.map_symbols(symbols,changes)
@@ -15,12 +17,15 @@ def main():
 
     while True :
         for i in range(len(sorted_symbols)):
+            # input keycode from Alpha Vantage
             json_extract = ws.api_call(sorted_symbols[i],'LO366YP95G58CFGT')
+
             if start[i] == True:
                 open_prices.append(ws.get_open_price(json_extract))
                 start[i] = False 
             data, recent_close = ws.cleanse_json(json_extract)
             recent_closes[i] = recent_close 
+
             # create an image for the Flutter app to retreive
             ws.plot(data, './dart_folder/assets/image' + str(i+1), i)
         
